81d26d45bbe4194bfe1620a7de9abca77d60c711
Alekseika2-1IS <lvovnanatala05@gmail.com>
2026-03-16 13:36:18 +0300

    продолжаю заполнять

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3001';
+
+export const getProducts = () => axios.get(`${API_URL}/products`);
+export const getProductById = (id) => axios.get(`${API_URL}/products/${id}`);
+export const createProduct = (product) => axios.post(`${API_URL}/products`, product);
+export const updateProduct = (id, product) => axios.put(`${API_URL}/products/${id}`, product);
+export const deleteProduct = (id) => axios.delete(`${API_URL}/products/${id}`);
